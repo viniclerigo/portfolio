30dce24caf949e2de59a040d0cf1c11f39050afb
viniclerigo <vnclerigo@gmail.com>
2026-02-19 17:25:50 -0300

    Testes
    Testes de scripts e análises.

--- assets/projetos/NutshellAPI/testes.py
+++ assets/projetos/NutshellAPI/testes.py
@@ -1,10 +1,9 @@
 import pandas as pd
 # import requests
 
-df = pd.read_json('pipelines.json')
+df = pd.read_json('assets/projetos/NutshellAPI/json/leads.json')
 
-# df.to_csv('teste.csv', index=False)
-
-print(df)
+# df.to_csv("assets/projetos/NutshellAPI/leads.csv", index=False)
+# df.to_excel("assets/projetos/NutshellAPI/leads.xlsx", index=False)
 
 print("fim")
